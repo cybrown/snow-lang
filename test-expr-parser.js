9b2100a61df55e554e6f9a3b3de2fb9ce3437c9e
Cy Brown <cy.brown59@gmail.com>
2017-09-26 12:20:30 +0200

    whitespaces

--- test-expr-parser.js
+++ test-expr-parser.js
@@ -1,27 +1,41 @@
 const fs = require('fs');
 const {parseExpression} = require('./lib/parser');
 
+let errors = 0;
+let total = 0;
+
 verify('Number_3', '3');
 verify('Number_34', '34');
 verify('Number_345', '345');
 verify('Number_3.14', '3.14');
 verify('Identifier_a', 'a');
 verify('Identifier_abcd', 'abcd');
-verify('Multiple_binaries', 'a+b-c+d');
-verify('Call_empty', 'abc()');
-verify('Call_one_args', 'a(3)');
-verify('Call_two_args', 'a(b,c)');
+verify('Multiple_binaries_1', 'a+b-c+d');
+verify('Multiple_binaries_2', ' a + b - c + d ');
+verify('Call_empty_1', 'abc()');
+verify('Call_empty_2', ' abc ( ) ');
+verify('Call_one_args_1', 'a(3)');
+verify('Call_one_args_2', ' a ( 3 ) ');
+verify('Call_two_args_1', 'a(b,c)');
+verify('Call_two_args_2', ' a ( b , c ) ');
 verify('Call_three_args', 'a(b,c,314)');
 verify('Multiple_calls_and_indices_1', 'a(b)[c]');
 verify('Multiple_calls_and_indices_2', 'a[b](c)[d](e)');
+verify('Multiple_calls_and_indices_3', ' a [ b ] ( c ) [ d ] ( e ) ');
 verify('Multiple_binaries_with_product', 'a+b*c-d');
 verify('Multiple_binaries_with_product_and_parenthesis_1', '(a+b)*c-d');
 verify('Multiple_binaries_with_product_and_parenthesis_2', '(a+b)*(c-d)');
+verify('Multiple_binaries_with_product_and_parenthesis_3', ' ( a + b ) * ( c - d ) ');
+//verify('If_simple', 'if a {b}else{c}');
+
+console.log(`${errors} errors, ${total} total`);
 
 function verify(name, expr) {
+    total++;
     const fileName = `./test/${name}.json`;
     const ast = parseExpression(expr);
     if (ast.status === false) {
+        errors++;
         console.log(`ERROR ${name}`)
         console.log(JSON.stringify(ast, null, '  '));
         return;
@@ -35,6 +49,7 @@ function verify(name, expr) {
         if (content === contentFromFile) {
             console.log(`OK ${name}`);
         } else {
+            errors++;
             console.log(`!! ${name}`);
         }
     }
