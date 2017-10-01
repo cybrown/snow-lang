576fff28d9933fc77cb7efbae37af225df5544f3
Cy Brown <cy.brown59@gmail.com>
2017-10-01 17:35:31 +0200

    Extracted source samples

--- test-parser.js
+++ test-parser.js
@@ -7,61 +7,13 @@ let mismatch = 0;
 let total = 0;
 let created = 0;
 
-verify('Number_3', '3');
-verify('Number_34', '34');
-verify('Number_345', '345');
-verify('Number_3.14', '3.14');
-verify('Identifier_a', 'a');
-verify('Identifier_abcd', 'abcd');
-verify('Multiple_binaries_1', 'a+b-c+d');
-verify('Multiple_binaries_2', ' a + b - c + d ');
-verify('Call_empty_1', 'abc()');
-verify('Call_empty_2', ' abc ( ) ');
-verify('Call_one_args_1', 'a(3)');
-verify('Call_one_args_2', ' a ( 3 ) ');
-verify('Call_two_args_1', 'a(b,c)');
-verify('Call_two_args_2', ' a ( b , c ) ');
-verify('Call_three_args', 'a(b,c,314)');
-verify('Multiple_calls_and_indices_1', 'a(b)[c]');
-verify('Multiple_calls_and_indices_2', 'a[b](c)[d](e)');
-verify('Multiple_calls_and_indices_3', ' a [ b ] ( c ) [ d ] ( e ) ');
-verify('Multiple_binaries_with_product', 'a+b*c-d');
-verify('Multiple_binaries_with_product_and_parenthesis_1', '(a+b)*c-d');
-verify('Multiple_binaries_with_product_and_parenthesis_2', '(a+b)*(c-d)');
-verify('Multiple_binaries_with_product_and_parenthesis_3', ' ( a + b ) * ( c - d ) ');
-verify('If_simple', 'if a { b } else { c }');
-verify('Multiple_expression', 'a ; 1+1; c; d');
-verify('Multiple_expression_in_subexpression', '(a ; 1+1; c; d)');
-verify('Multi_expression_program_with_function_definition', `
-    a;
-    fun abc ( a, b ) { toto };
-    b
-`);
-verify('Assignement_global', `a = a + 5; a`);
-verify('Assignement_parameter', `
-    fun abc(bfg) {
-        bfg = bfg + 5;
-        bfg
-    };
-    abc(5)
-`);
-verify('Local_variables', `
-    fun abc(bfg) {
-        var a;
-        a = 6;
-        var b;
-        b = bfg + a;
-        b
-    };
-    abc(5)
-`);
-verify('NativeOperation_1', '#ADD(2, #MUL(4, 8))');
+const files = fs.readdirSync('./test-samples-sources');
 
-console.log(`Parser test report: ${parseError} errors, ${mismatch} mismatches, ${created} created, ${total} total`);
-
-function verify(name, expr) {
+files.forEach(name => {
     total++;
-    const fileName = `./test-samples-parser/${name}.yml`;
+    const sourcePath = `./test-samples-sources/${name}`;
+    const fileName = `./test-samples-parser/${name.substring(0, name.length - 5)}.yml`;
+    const expr = fs.readFileSync(sourcePath, {encoding: 'UTF-8'});
     const ast = parseProgram(expr);
     if (ast.status === false) {
         parseError++;
@@ -81,4 +33,6 @@ function verify(name, expr) {
             console.log(`MISMATCH ${name}`);
         }
     }
-}
+});
+
+console.log(`Parser test report: ${parseError} errors, ${mismatch} mismatches, ${created} created, ${total} total`);
